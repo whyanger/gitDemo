4467464d3913bd83423490fd14e02f4b530a6cae
天净沙 <whyanger@163.com>
2017-09-30 13:45:07 +0800

    Add button to UI that logs to console when tapped

--- gitDemo/ViewController.swift
+++ gitDemo/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var example: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func touchUpExample(_ sender: UIButton) {
+        print("this is a git example")
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
